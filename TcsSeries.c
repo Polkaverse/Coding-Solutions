9a97df8ab7a285367c51ddb97d93ba0499dfc456
PANKAJ CHAUDHARY <pankajchaudhary172@gmail.com>
2018-09-02 12:12:28 +0530

    Create TcsSeries.c

---
+++ TcsSeries.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(int argc , char * argv[])
+{
+    int a , b , i ;
+    a=atoi(argv[1]);
+    if(a%2==0)
+    {
+        b=(a/2)-1;
+        i=pow(3,b);
+        printf("%d",i);
+    }
+    else
+    {
+        b=(a/2);
+        i=pow(2,b);
+        printf("%d",i);
+    }
+
+    return 0;
+}
